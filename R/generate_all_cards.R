1ec4e25041270ec6a4e0af49a1c70de5acd3d618
fheitmueller <fheitmueller@gmx.de>
2020-12-29 23:49:53 +0100

    rewrite as function that takes pre-loaded image list

--- R/generate_all_cards.R
+++ R/generate_all_cards.R
@@ -1,30 +1,26 @@
 #' Generate all cards
 #'
 #' This function generates all dobble cards with the help of the generate one card function
-#' @param infolder
-#' @param outfolder is the path of a folder in which the 57 created cards are saved
-#' @param x is a list of dobble card templates
+#' @param image_list
 #' @return a set of dobble cards saved in the outfolder
-#' @import magick
-#' @import grDevices
-#' @import stats
 #' @export
 
 
-generate_all_cards <- function(infolder, saving=FALSE, outfolder){
-file_list <- list.files(path=infolder)
+generate_all_cards <- function(image_list){
 
   # warnings and errors
 
-  if (length(file_list) < 57){
-    stop("Number of pictures is smaller than 57. Make sure that 57 images are in the specified folder")
+  if (length(image_list) < 57){
+    stop("Number of pictures is smaller than 57. Make sure that 57 images are in the list")
   }
 
-  if (length(file_list) > 57){
+  if (length(image_list) > 57){
     warning("More than 57 images were supplied. The first 57 images in the folder are used")
   }
 
-  ##create a list of different possible positions within the cell
+image_list <- lapply(image_list, image_clean)
+
+##create a list of different possible positions within the cell
   positions <- as.data.frame(rbind(
     c(6, 8.5),
     c(8.5, 6),
@@ -34,14 +30,12 @@ file_list <- list.files(path=infolder)
     c(3, 1.3),
     c(1.5, 3),
     c(1.5, 6)))
+
 cardlist <- dobble_template()
-cardlist <- list(c(1:8), (9:16))
 
-all_cards <- lapply(cardlist, generate_one_card,infolder=infolder, file_list=file_list, positions=positions)
 
-if(saving==TRUE){
-  lapply(seq_along(all_cards), function(y, i){image_write(y[[i]], path=paste0(outfolder, "card",i, ".png"))}, y=all_cards)
-}
+all_cards <- lapply(cardlist, generate_one_card,image_list=image_list, positions=positions)
+
 
 return(all_cards)
 }
